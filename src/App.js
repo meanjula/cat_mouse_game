beed90bc37f175b0d07e8eec28f9a436b9a96d07
meanjula <anjula.ghale@gmail.com>
2021-11-15 18:33:58 +0200

    created base component and functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 
 import './App.css';
+import React, { Component } from 'react';
+import {circles} from './components/circleData';
+import Circle from './components/Circle';
 
-function App() {
-  return (
-    <div className="App">
-
+class App extends Component {
+  state={
+    count:0,
+  };
+  handleClick=()=>{
+    this.setState({
+      count:this.state.count+10,
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+      <h1>CatMouse Game</h1>
+      <p>Your score</p>
+      <div className="circles">
+      {circles.map(c =>(
+        <Circle key={c.id} color={c.color} id={c.id} onClick={this.handleClick}/>
+      ))}
+      </div>
+        <button className="startBtn">Start</button>
+        <button>Stop</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
